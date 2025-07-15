3444ea0b8d526334a8acf1cd8da142b9bc5ce993
Ramiro <ramiatorres@outlook.com>
2025-07-15 11:59:14 +0200

    2 - Finished

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         ArrayList<Object> list = new ArrayList<>();
-        for(int i = 20; i <= 50; i++) {
+        for (int i = 20; i <= 50; i++) {
             list.add(i);
             System.out.println(i);
         }
 
-        System.out.println("Final list " + list);
+        System.out.println("Final list: " + list);
+
+        ArrayList<String> jordiList = new ArrayList<>();
+        String item;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Add a typical elements of the Sant Jordi festival.\n" +
+                    "To exit, press S.");
+            item = sc.nextLine();
+            if (!item.equalsIgnoreCase("S")) {
+                jordiList.add(item);
+            }
+        } while (!item.equalsIgnoreCase("S"));
+
+        System.out.println("The Sant Jordi list includes :" + jordiList);
     }
 }
